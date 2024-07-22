52e66478c1f90767fdacfe45c2e08aad1a0dbe16
Ashutosh Jha <108254506+ashu7228@users.noreply.github.com>
2024-07-22 12:58:15 +0530

    Made login and signup components and connected to db

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Homepage.css";
+
+const Homepage = () => {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage;
---
+++ src/Components/Registration/Register .jsx
@@ -0,0 +1,58 @@
+// src/Register.js
+import React, { useState } from "react";
+import axios from "axios";
+import "./Register.css";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/register", {
+        username,
+        email,
+        password,
+      });
+    } catch (error) {
+      console.error("There was an error registering the user!", error);
+    }
+  };
+
+  return (
+    <div className="register-container">
+      <form onSubmit={handleSubmit}>
+        <h1>Registration</h1>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
